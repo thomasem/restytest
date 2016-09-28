e31e0f4ff2bfa095293e1d1b99822374e77c30e3
Thomas Maddox <thomas.e.maddox@gmail.com>
2016-09-28 08:34:49 -0500

    Implement initial project layout

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils import core
+import os
+from pip import req
+import setuptools
+
+requirements = [
+    str(r.req) for r in req.parse_requirements('requirements.txt',
+                                               session=False)
+]
+
+core.setup(
+    name='RestyTest',
+    version='0.0.1',
+    description="Example REST application exposing the relationship between"
+                " Users and Groups.",
+    author='Thomas Maddox',
+    author_email='thomas.e.maddox@gmail.com',
+    license="GPLv3",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+)
